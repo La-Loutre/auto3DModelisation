47d9c5400a68a6a3d2f4ca38d19e2b08bb8cf7da
Louis Douriez <ldouriez@gmail.com>
2015-01-25 16:10:07 +0100

    added distance calcul

--- testPython.py
+++ testPython.py
@@ -3,6 +3,17 @@ from PIL import Image,ImageTk
 import tkFileDialog
 import Tkinter
 import Image
+import math
+MAXWIDTH=1000
+
+camera0Angle=135
+camera0AngleMort=180-camera0Angle
+
+camera1Angle=135
+camera1AngleMort=180-camera1Angle
+
+widthCamera=0.2
+
 
 class Application:
     def __init__(self,master):
@@ -36,18 +47,25 @@ class Application:
     def addPoint1(self,event):
         canvasTmp=self.canvasImg1
         point=self.point1
-        point[0]=int(event.x)
-        point[1]=int(event.y)
+        point[0]=int(event.x)/self.ratio1
+        point[1]=int(event.y)/self.ratio1
         print(self.point0,self.point1)
+        print(angle(self.img1.size[0],self.point1[0],camera1Angle))
+        dY=distanceY(angle(self.img0.size[0],self.point0[0],camera0Angle),angle(self.img1.size[0],self.point1[0],camera1Angle),widthCamera)
+        print(dY)
+        dX=distanceX(angle(self.img0.size[0],self.point0[0],camera0Angle),angle(self.img1.size[0],self.point1[0],camera1Angle),widthCamera,dY)
+        print dX
+
+
         
     def addPoint0(self,event):
         canvasTmp=self.canvasImg0
         point=self.point0
         
-        point[0]=int(event.x)
-        point[1]=int(event.y)
+        point[0]=int(event.x)/self.ratio0
+        point[1]=int(event.y)/self.ratio0
         print(self.point0,self.point1)
-        
+        print(angle(self.img0.size[0],self.point0[0],camera0Angle))
     
     def createFileMenu(self):
         self.fileMenu=Menu(self.baseMenu)
@@ -68,10 +86,10 @@ class Application:
         
     def refreshPicture(self,img,pictureNumber=0):
         (x,y)=img.size
-        MAXWIDTH=400
         if(pictureNumber==0):
             self.img0=img
             ratio = MAXWIDTH/float(x); 
+            self.ratio0=ratio
             height=int(float(y)*float(ratio))
             self.img0Thumbnail= img.resize((MAXWIDTH,height), Image.ANTIALIAS)
             canvasTmp=self.canvasImg0
@@ -82,6 +100,7 @@ class Application:
         else:
             self.img1=img
             ratio = MAXWIDTH/float(x); 
+            self.ratio1=ratio
             height=int(float(y)*float(ratio))
             self.img1Thumbnail= img.resize((MAXWIDTH, height), Image.ANTIALIAS)
             canvasTmp=self.canvasImg1
@@ -89,12 +108,30 @@ class Application:
             self.photo1=ImageTk.PhotoImage(self.img1Thumbnail)
             photoTmp=self.photo1
             (x1,y1)=self.img1Thumbnail.size
-            self.img1Thumbnail.show()
+            
     
         canvasTmp.create_image((x1/2,y1/2),image=photoTmp)
         canvasTmp.config(width=x1,height=y1,scrollregion=(0,0,x1,y1))
         canvasTmp.pack(side=sideTmp)
         self.frame.pack()
+
+def distanceX(alpha,beta,width,dY):
+    print(alpha,beta,width)
+    if(alpha*beta >0 ):
+        return (dY*float(math.tan(alpha))+dY*float(math.tan(beta)))/float(2)
+    return (-dY*float(alpha)+dY*float(math.tan(beta))+width)/float(2)
+    
+def distanceY(alpha,beta,width):
+    print(alpha,beta,width)
+    print(math.tan(alpha),math.tan(beta))
+    if(alpha*beta >0 ):
+        return abs(float(width)/float((math.tan(alpha)-math.tan(beta))))
+    return abs(float(width)/float((math.tan(alpha)+math.tan(beta))))
+    
+def angle(widthImg,posXPoint,cameraAngle):
+#    r=90-(camera0AngleMort+(width-posYPoint)*camera0Angle/widthImg)
+
+    return (posXPoint*cameraAngle/float(widthImg)-cameraAngle/float(2)) * math.pi/float(180)
         
 root = Tk()
 application = Application(root)
